3eb1a2e7fa2f7f0ed71beeaaa06d58b1ffbbcf42
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-10-21 10:32:40 +0100

    Recreate as Beta 8 project extra files

---
+++ src/jspmdnx/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("jspmdnx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acquis Insurance")]
+[assembly: AssemblyProduct("jspmdnx")]
+[assembly: AssemblyCopyright("Copyright © Acquis Insurance 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f5977442-1fcf-4d41-9c50-68407a4183c9")]
