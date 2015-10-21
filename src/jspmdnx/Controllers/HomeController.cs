fe88aca631b08dd656f0c54e87c024f84c15dce7
Jonathon Douglas <jdouglas@acquisinsurance.com>
2015-10-21 10:32:21 +0100

    Recreate as a proper beta8 project layout

---
+++ src/jspmdnx/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿namespace jspmdnx.Controllers
+{
+  using System;
+  using Microsoft.AspNet.Mvc;
+
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      ViewData["Message"] = "Your application description page.";
+
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      ViewData["Message"] = "Your contact page.";
+
+      return View();
+    }
+
+    public IActionResult Error()
+    {
+      return View("~/Views/Shared/Error.cshtml");
+    }
+  }
+}
---
+++ src/jspmdnx/Startup.cs
@@ -0,0 +1,53 @@
+﻿namespace jspmdnx
+{
+  using System;
+  using Microsoft.AspNet.Builder;
+  using Microsoft.AspNet.Hosting;
+  using Microsoft.Dnx.Runtime;
+  using Microsoft.Framework.Configuration;
+  using Microsoft.Framework.DependencyInjection;
+  using Microsoft.Framework.Logging;
+
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+    {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(appEnv.ApplicationBasePath)
+        .AddJsonFile("config.json")
+        .AddEnvironmentVariables();
+      Configuration = builder.Build();
+    }
+
+    public IConfiguration Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.UseIISPlatformHandler();
+      app.UseRuntimeInfoPage();
+      loggerFactory.MinimumLevel = LogLevel.Information;
+
+      if (env.IsDevelopment())
+      {
+        loggerFactory.AddConsole();
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
+}
